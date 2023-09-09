6bf8b7b075be5894897cd57dc34647bc304494a9
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-09-09 14:56:35 -0700

    Ensured usernames and user emails are all lower case for uniformity. Also added indexing to username field

--- socialpy/settings.py
+++ socialpy/settings.py
@@ -32,6 +32,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Allow requests from any origin during development
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_HEADERS = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Application definition
 
@@ -62,6 +67,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -207,7 +213,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+<<<<<<< Updated upstream
 STATIC_URL = 'static/'
+=======
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'socialpy-frontend', 'dist'),
+]
+>>>>>>> Stashed changes
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
