5d017a43c86b1602abf283c5ea7bb025932fa7c4
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-09-14 19:43:08 -0700

    Added new components to module page

--- socialpy-frontend/src/app/app.module.ts
+++ socialpy-frontend/src/app/app.module.ts
@@ -20,6 +20,11 @@ import { UserService } from './api-services/user/user.service';
 import { PostService } from './api-services/post/post.service';
 
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
+import { HomeComponent } from './home/home.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+=======
 import { HomeComponent } from './components/home/home.component';
 import { UserSignupComponent } from './components/user-signup/user-signup.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
@@ -27,6 +32,9 @@ import { UserProfileComponent } from './components/user-profile/user-profile.com
 
 import { UserFeedEffects } from './store/user-feed/user-feed.effects';
 import { PostActionsEffects } from './store/post-actions/post-actions.effects';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { LoadingPageComponent } from './loading-page/loading-page.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -34,7 +42,12 @@ import { PostActionsEffects } from './store/post-actions/post-actions.effects';
     HomeComponent,
     UserSignupComponent,
     UserLoginComponent,
+<<<<<<< Updated upstream
+=======
     UserProfileComponent,
+    ErrorPageComponent,
+    LoadingPageComponent,
+>>>>>>> Stashed changes
   ],
   imports: [
     HttpClientModule,
