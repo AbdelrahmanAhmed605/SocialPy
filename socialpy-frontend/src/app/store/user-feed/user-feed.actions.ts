efc1f7e09dcf62ac569ca25aa31089cfe6106b89
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-09-18 16:07:22 -0700

    Add page and hasMoreData props to user feed actions

--- socialpy-frontend/src/app/store/user-feed/user-feed.actions.ts
+++ socialpy-frontend/src/app/store/user-feed/user-feed.actions.ts
@@ -1,13 +1,16 @@
 import { createAction, props } from '@ngrx/store';
-import { Post } from './user-feed.model';
+import { Post } from './user-feed.model'; // Represents the structure of a post object received from the API.
 
 // Action to initiate fetching the user's feed
-export const loadUserFeed = createAction('[User Feed] Load User Feed');
+export const loadUserFeed = createAction(
+  '[User Feed] Load User Feed',
+  props<{ page: number }>() // page prop refers to page of the paginated api data
+);
 
 // Action to handle successful fetching of the user's feed
 export const loadUserFeedSuccess = createAction(
   '[User Feed] Load User Feed Success',
-  props<{ postData: Post[] }>() // Use the Post[] type for postData
+  props<{ postData: Post[]; hasMoreData: boolean }>() // hasMoreData checks api call to see if there is more paginated data
 );
 
 // Action to handle failure when fetching the user's feed
