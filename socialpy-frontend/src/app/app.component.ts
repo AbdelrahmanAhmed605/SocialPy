9b5f6a0c029e9b7d73df11ff48941020ddeaaa8f
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-09-19 17:05:42 -0700

    Apply Correct Theme on App Initialization

--- socialpy-frontend/src/app/app.component.ts
+++ socialpy-frontend/src/app/app.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './store/app.state';
+import { getAppTheme } from './store/app-theme/app-theme.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    // Dispatch the getAppTheme action when the app starts.
+    // The effect middleware of the action will set the theme to the document body
+    this.store.dispatch(getAppTheme());
+  }
+}
