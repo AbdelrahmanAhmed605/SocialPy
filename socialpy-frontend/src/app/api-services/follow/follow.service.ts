e48a1456ff05aadcd9f88ed64fcd7f627c78e851
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-09-21 18:58:02 -0700

    Added FollowService with followUser function

---
+++ socialpy-frontend/src/app/api-services/follow/follow.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { switchMap, catchError } from 'rxjs/operators';
+
+import { API_BASE_URL } from '../api.config';
+import { AuthService } from 'src/utilities/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FollowService {
+  constructor(private http: HttpClient, private authService: AuthService) { }
+  
+  followUser(userId: number): Observable<any> {
+    const followUserEndpoint = `follow/user/${userId}/`;
+
+    return this.authService.handleAuthenticationToken().pipe(
+      switchMap((token) => {
+        // Set the headers with the Authorization token
+        const headers = new HttpHeaders({
+          Authorization: `Token ${token}`,
+        });
+
+        // Like the specified post
+        return this.http.post(`${API_BASE_URL}/${followUserEndpoint}`, null, {
+          headers,
+          withCredentials: true,
+        });
+      }),
+      catchError((error) => {
+        // throw the error
+        return throwError(() => error);
+      })
+    );
+   }
+
+}
