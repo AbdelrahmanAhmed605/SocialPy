793b10af5bd551f7aa678e0abe60c883b16362a2
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-09-21 22:00:44 -0700

    Add unfollow user service function

--- socialpy-frontend/src/app/api-services/follow/follow.service.ts
+++ socialpy-frontend/src/app/api-services/follow/follow.service.ts
@@ -11,8 +11,9 @@ import { AuthService } from 'src/utilities/auth';
   providedIn: 'root',
 })
 export class FollowService {
-  constructor(private http: HttpClient, private authService: AuthService) { }
-  
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  // API call to follow a user
   followUser(userId: number): Observable<any> {
     const followUserEndpoint = `follow/user/${userId}/`;
 
@@ -34,6 +35,29 @@ export class FollowService {
         return throwError(() => error);
       })
     );
-   }
+  }
 
+  // API call to unfollow a user
+  unfollowUser(userId: number): Observable<any> {
+    const unfollowUserEndpoint = `unfollow/user/${userId}/`;
+
+    return this.authService.handleAuthenticationToken().pipe(
+      switchMap((token) => {
+        // Set the headers with the Authorization token
+        const headers = new HttpHeaders({
+          Authorization: `Token ${token}`,
+        });
+
+        // Like the specified post
+        return this.http.post(`${API_BASE_URL}/${unfollowUserEndpoint}`, null, {
+          headers,
+          withCredentials: true,
+        });
+      }),
+      catchError((error) => {
+        // throw the error
+        return throwError(() => error);
+      })
+    );
+  }
 }
