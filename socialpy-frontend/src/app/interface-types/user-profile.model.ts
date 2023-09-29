f5916b8b73340e85622ac948dbab244f2d9b9292
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-09-29 16:07:03 -0700

    Create interface for structure of results from user profile api service function

---
+++ socialpy-frontend/src/app/interface-types/user-profile.model.ts
@@ -0,0 +1,12 @@
+// Represents the structure of a user profile object received from the API.
+export interface UserProfileResponse {
+  bio: string;
+  can_view: boolean;
+  follow_status: string | boolean;
+  num_followers: number;
+  num_following: number;
+  num_posts: number;
+  posts: Array<{ id: number; media: string }>;
+  profile_picture: string | null;
+  username: string;
+}
